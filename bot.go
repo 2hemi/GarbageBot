cf26e6c9a7239ad9c10c169094fa138d018ea64a
Azrotronik <60074481+Azrotronik@users.noreply.github.com>
2021-04-04 04:14:12 +0100

    Refactored.

--- bot.go
+++ bot.go
@@ -1,9 +1,4 @@
 package main
-import (
-	"log"
-	"crypto/tls"
-	"strings"
-)
 
 type ircmessage struct{
 	 sender     string
@@ -17,68 +12,38 @@ type ircserver  struct{
 }
 type ircchan    struct{
 	 name       string
-	 users    []string	
+	 users    []ircuser	
 }               
 type ircbotid   struct{
 	 user	    string
 	 nick 	    string
 	 rname      string
 }
+  
+type ircuser    struct{
+	 nick 	   	string
+	 rname      string
+	 prefix		byte
+}
+
+var (chanlist  	[]ircchan	  
+	exserver	ircserver
+	exbot		ircbotid
+	servername	string)
 
 func main(){
-	var chanlist  []ircchan	
- chanlist = append(chanlist, ircchan{name:"#ex1"})
- chanlist = append(chanlist, ircchan{name:"#ex2"})
- 				  
+
+	chanlist = append(chanlist, ircchan{name:"#ex1"})
+	chanlist = append(chanlist, ircchan{name:"#ex2"})
 	exserver    :=ircserver{
 				  hostname:"irc.rizon.net",
 				  port:    "6697",
 				  channels:chanlist} 
-	
 	exbot		:=ircbotid{
-				  user: "GarbageBot",
-				  nick: "GarbageBot",
-				  rname:"GarbageBot"}
-
-	maincon :=ircconnect(exserver.hostname,exserver.port,exbot.user+" "+exbot.user+" "+exbot.user+" "+exbot.rname,exbot.nick)
-	ircmaintain(maincon,exserver.channels)
-
+				  user: "Garbage",
+				  nick: "Garbage",
+				  rname:"Garbage"}
+	maincon,servername :=ircconnect(exserver.hostname,exserver.port, exserver.channels, exbot.user+" "+exbot.user+" "+exbot.user+" "+exbot.rname,exbot.nick)
+	ircmaintain(maincon,exserver.channels,servername)
 }
 
-//Connect
-func ircconnect(server string,port string,user string,nick string) *tls.Conn{
-	log.Println("Gossipbot: Initializing connection.")
-    tlsconf := &tls.Config{}
-	conn, err := tls.Dial("tcp", server+":"+port,tlsconf)
-	if err != nil {log.Fatalln(err)}	
-    irccommand("USER",user,conn)
-	irccommand("NICK",nick,conn)
-
-   	return conn
-}
-//Maintain connection, main infinite loop.
-func ircmaintain(irccon *tls.Conn,chanlist []ircchan){
-	defer irccon.Close()
-	stream := make([]byte, 256)
-		
-	for {
-			linelen, err := irccon.Read(stream)
-			if err != nil {log.Fatal(err)}
-		log.Print(string(stream[:linelen]))
-			
-			if string(stream[:4]) =="PING" { 				
-				pong(linelen,string(stream),irccon)
-
-			}else if strings.Contains(string(stream[:linelen]), " 372 "){
-			//	Completely ignore everything in the MOTD as it breaks stuff.
-			
-			}else if strings.Contains(string(stream[:linelen]), " PRIVMSG #"){ //TODO handle PMs and Messages SEPERATELY
-					handleprivmsg(linelen,string(stream),irccon)
-
-			}else if strings.Contains(string(stream[:linelen]), " 001 "){
-					irccommand("VERSION","Bot",irccon)
-					joinchan(irccon,chanlist)
-				
-			}
-		}	
-}
--- funcs.go
+++ funcs.go
@@ -5,13 +5,76 @@ import (
 	"strings"
 )
 
+func handleline(linelen int, stream string, servername string, irccon *tls.Conn){
+	log.Print(stream[:linelen])
+
+	if string(stream[:4]) == "PING" {pong(linelen,string(stream),irccon)}
+	
+	if strings.Contains(stream[:2+len(servername)],":"+servername+" "){
+		if strings.Contains(stream[2+len(servername):linelen],"001 "){
+			handlecode(001,"",linelen,irccon)
+		}else if strings.Contains(stream[2+len(servername):linelen],"433 "){
+			handlecode(433,"",linelen,irccon)
+		}else if strings.Contains(stream[2+len(servername):linelen],"353 "){
+			handlecode(353,stream,linelen,irccon)
+		}
+		
+	}
+
+/*	if strings.Contains(stream[2+len(servername):linelen],"PRIVMSG GarbageBot :"){
+		handleprivmsg(linelen,stream)
+	}*/
+
+}
+//Respond to server codes
+func handlecode(code int,stream string,linelen int, irccon *tls.Conn){
+   switch code {
+      	case 001:
+			irccommand("VERSION","Bot",irccon)
+      		joinchan(irccon,chanlist);      	
+      	case 433:
+      		log.Fatal("GarbageBot: it seems that someone is sqatting this nick, change it and reconnect.");
+		case 353:
+      		for strings.Contains(stream[2+len(servername):linelen],"366 "){
+				//TODO irccon read and slice
+			};
+   }
+}
+//Connect to a server
+func ircconnect(server string,port string, chanlist []ircchan,user string,nick string) (*tls.Conn,string){
+	log.Println("GarbageBot: Initializing connection.")
+	stream := make([]byte, 256)	
+    tlsconf := &tls.Config{}
+	irccon, err := tls.Dial("tcp", server+":"+port,tlsconf)
+	if err != nil {log.Fatalln(err)}	
+
+	linelen, err := irccon.Read(stream)
+	if err != nil {log.Fatal(err)}
+	servername := string(stream[1:strings.Index(string(stream[:linelen])," ")])	
+
+	irccommand("USER",user,irccon)
+	irccommand("NICK",nick,irccon)
+
+   	return irccon, servername
+}
+//Maintain the connection, main infinite loop.
+func ircmaintain(irccon *tls.Conn,chanlist []ircchan, servername string){
+	defer irccon.Close()
+	stream := make([]byte, 256)	
+	for {
+		linelen, err := irccon.Read(stream)
+		if err != nil {log.Fatal(err)}
+		handleline(linelen, string(stream), servername, irccon)		
+	}	
+}
+
 //Respond to PINGs
 func pong(linelen int,stream string,conn *tls.Conn){
 	conn.Write([]byte("PONG "+stream[strings.Index(stream," :")+1:linelen]+"\r\n"))
 	log.Print("PONG "+stream[strings.Index(stream," :")+1:linelen])
 }
 //Respond to PRIVMSG
-func handleprivmsg(linelen int,stream string,conn *tls.Conn){
+func handleprivmsg(linelen int,stream string){
 	var m ircmessage
 	m.sender=stream[1: strings.Index(stream,"!")]
 	m.recipient=stream[strings.Index(stream,"PRIVMSG ")+7:strings.Index(stream," :")]
@@ -33,10 +96,10 @@ func irccommand(command string, params string,conn *tls.Conn){
 //Join channel
 func joinchan(irccon *tls.Conn, chanlist []ircchan){
 	for i:=0;i<len(chanlist);i++ {
-					irccommand("JOIN",chanlist[i].name,irccon)
-					privmsg(chanlist[i].name,"Ready.",irccon)
-					log.Println("GarbageBot: I'm in "+chanlist[i].name)
-					}//TODO Fill out NAMES from NAMES list
+		irccommand("JOIN",chanlist[i].name,irccon)
+		privmsg(chanlist[i].name,"Ready.",irccon)
+		log.Println("GarbageBot: I'm in "+chanlist[i].name)
+	}
 	
 }
 
