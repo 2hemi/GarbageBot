0b61067af1a50820f2df6e9b0e88c134c5e4bfbf
Azrotronik <60074481+Azrotronik@users.noreply.github.com>
2021-04-08 22:00:45 +0000

    added

---
+++ modules.go
@@ -0,0 +1,11 @@
+package main
+
+import "crypto/tls"
+
+func modules(sender string, recipient string, message string, irccon *tls.Conn) {
+	report(sender, recipient, message, irccon)
+}
+
+func modulespm(sender string, message string, irccon *tls.Conn) {
+	reportpm(sender, message, irccon)
+}
